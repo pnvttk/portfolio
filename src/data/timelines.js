722bfaf58cc80230f813edad770b2dff34032e3f
Pannavach Tratongkam <pannavach@outlook.com>
2023-03-17 15:00:15 +0700

    fix: typo

--- src/data/timelines.js
+++ src/data/timelines.js
@@ -6,7 +6,7 @@ export default [
         detail: "After have work as outsource (contract) in backend developer position. They offer me to continue the project in full-stack developer position, and now I'm currently working on frontend and backend integration with JavaScript."
     },
     {
-        year: 'April 2022 - Octover 2022',
+        year: 'April 2022 - October 2022',
         title: 'Backend Developer at Siam Cement Group (SCG)',
         duration: '7 month (outsource (contract) / work from home)',
         detail: 'I have worked on a project about vehicle reservation management on web application. The stack is LAMP Stack and focus more into PHP backend, MySQL database and system design.'
