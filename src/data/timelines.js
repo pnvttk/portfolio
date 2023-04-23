142ecb8a864b6fc01ea8a762b0b102bcb39bed5e
Pannavach Tratongkam <pannavach.t@outlook.com>
2023-04-23 21:42:53 +0700

    fix: change intro and update timelines

--- src/data/timelines.js
+++ src/data/timelines.js
@@ -1,32 +1,43 @@
 export default [
-    {
-        year: 'December 2022 - March 2023',
-        title: 'Full-stack Developer at Siam Cement Group (SCG)',
-        duration: '4 month (Internship)',
-        detail: "After have work as outsource (contract) in backend developer position. They offer me to continue the project in full-stack developer position, and now I'm currently working on frontend and backend integration with JavaScript."
-    },
-    {
-        year: 'April 2022 - October 2022',
-        title: 'Backend Developer at Siam Cement Group (SCG)',
-        duration: '7 month (outsource (contract) / work from home)',
-        detail: 'I have worked on a project about vehicle reservation management on web application. The stack is LAMP Stack and focus more into PHP backend, MySQL database and system design.'
-    },
-    {
-        year: 'June 2019',
-        title: 'Hopped back into Computer Engineer at RMUTSV.',
-        duration: '4 year',
-        detail: "After have some thought, I decide to follow my dream and went to programmer path. Lucky, I passed into Computer Engineer at RMUTSV with my portfolio from Computer Graphic and here I am now."
-    },
-    {
-        year: '~2016',
-        title: 'Hopping into Computer Graphic at Songkhla Vocational College.',
-        duration: '3 year',
-        detail: "I had interested in video editing at that time, so I thought why not go study for it. And that's how I have learned some graphic design and video editing."
-    },
-    {
-        year: '~2012',
-        title: 'My first code at YORSOR school.',
-        duration: '3 year',
-        detail: "Actually, I don't remember when was my first code. As a kid I always interested in computer and at that time they have Mathematic Computer and Abilities Program special class or something, I don't know what they call in English. So I went for it and I had learned C, Adobe Dreamweaver and write my HTML code in notepad."
-    },
-]
+  {
+    year: "April 2023 - Now",
+    title: "Software Engineer",
+    duration: "-",
+    detail: "-",
+  },
+  {
+    year: "December 2022 - March 2023",
+    title: "Full-stack Developer at Siam Cement Group (SCG)",
+    duration: "4 month (Internship)",
+    detail:
+      "After have work as outsource (contract) in backend developer position. They offer me to continue the project in full-stack developer position, and now I'm currently working on frontend and backend integration with JavaScript.",
+  },
+  {
+    year: "April 2022 - October 2022",
+    title: "Backend Developer at Siam Cement Group (SCG)",
+    duration: "7 month (outsource (contract) / work from home)",
+    detail:
+      "I have worked on a project about vehicle reservation management on web application. The stack is LAMP Stack and focus more into PHP backend, MySQL database and system design.",
+  },
+  {
+    year: "June 2019",
+    title: "Hopped back into Computer Engineer at RMUTSV.",
+    duration: "4 year",
+    detail:
+      "After have some thought, I decide to follow my dream and went to programmer path. Lucky, I passed into Computer Engineer at RMUTSV with my portfolio from Computer Graphic and here I am now.",
+  },
+  {
+    year: "~2016",
+    title: "Hopping into Computer Graphic at Songkhla Vocational College.",
+    duration: "3 year",
+    detail:
+      "I had interested in video editing at that time, so I thought why not go study for it. And that's how I have learned some graphic design and video editing.",
+  },
+  {
+    year: "~2012",
+    title: "My first code at YORSOR school.",
+    duration: "3 year",
+    detail:
+      "Actually, I don't remember when was my first code. As a kid I always interested in computer and at that time they have Mathematic Computer and Abilities Program special class or something, I don't know what they call in English. So I went for it and I had learned C, Adobe Dreamweaver and write my HTML code in notepad.",
+  },
+];
